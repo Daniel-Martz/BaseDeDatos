653bc0514ed15163b8e11d52b5e1916c85cf4de4
Daniel-Martz <danieluniversidad006@gmail.com>
2025-10-11 16:30:46 +0200

    cambio

--- nuevabase.sql
+++ nuevabase.sql
@@ -1,15 +1,19 @@
-CREATE TABLE seats_new AS
-SELECT f.flight_id, s.seat_no, s.fare_conditions
-FROM flights f
-JOIN seats s ON f.aircraft_code = s.aircraft_code;
+CREATE TABLE seats_new AS 
+  SELECT f.flight_id, 
+         s.seat_no, 
+         s.fare_conditions 
+  FROM   flights f 
+         JOIN seats s 
+           ON f.aircraft_code = s.aircraft_code; 
 
-ALTER TABLE seats_new
-ADD CONSTRAINT seats_new_pkey PRIMARY KEY (flight_id, seat_no);
+ALTER TABLE seats_new 
+  ADD CONSTRAINT seats_new_pkey PRIMARY KEY (flight_id, seat_no); 
 
-ALTER TABLE seats_new
-ADD CONSTRAINT seats_new_flight_id_fkey
-FOREIGN KEY (flight_id) REFERENCES flights(flight_id);
+ALTER TABLE seats_new 
+  ADD CONSTRAINT seats_new_flight_id_fkey FOREIGN KEY (flight_id) REFERENCES 
+  flights(flight_id); 
 
-DROP TABLE seats CASCADE;
-ALTER TABLE seats_new RENAME TO seats;
+DROP TABLE seats CASCADE; 
 
+ALTER TABLE seats_new 
+  RENAME TO seats;
